cec574368836090fee410b9575c2cb1903520ece
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-02-02 22:48:56 +0000

    adding lib folder and mysql jar for jenkins
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs-grails-plugins/grails-ncs-people/trunk@241 87bbd5ad-132b-4bfb-b67c-16c9f46f48d1

--- NcsPeopleGrailsPlugin.groovy
+++ NcsPeopleGrailsPlugin.groovy
@@ -7,7 +7,8 @@ class NcsPeopleGrailsPlugin {
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+			"lib/"
     ]
 
     // TODO Fill in these fields
